71fa732a04fec47fd4621ba0cc0c21fd8d704132
Vilhelm <47038708+ViPa222dv@users.noreply.github.com>
2021-06-07 21:13:02 +0200

    Just trying some stuff out for now, work more tomorrow

--- a01-web-scraper/js/restaurantScaper.js
+++ a01-web-scraper/js/restaurantScaper.js
@@ -3,13 +3,31 @@ const cheerio = require('cheerio')
 
 const restaurantScraper = {
     loginToBooking: async function(url) {
-        const getData = {
-            method: 'Get'
+        const login ={
+            username: 'zeke',
+            password: 'coys',
+            submit: "login"
         }
-        const response = await fetch(url, getData)
-        const data = await response.text()
-        let $=cheerio.load(data)
-        console.log($.html())
+        try {
+            await fetch(url + '/login'), {
+                headers: 'Content-Type: text/html; charset=utf-8',                
+                method: 'post',
+                body: login
+            }.then((response)=>{  
+                if(response.ok){
+                    console.log('The request is ok.')
+                    return
+                }
+                throw new Error(`We have a error! Status: ${response.status}`)
+            })
+            /*const response = await fetch(url, getData)
+            const data = await response.text()
+            let $=cheerio.load(data)
+            console.log($.html())*/
+        }
+        catch (error) {
+            throw new Error(error)
+        }
     }
 }
 
