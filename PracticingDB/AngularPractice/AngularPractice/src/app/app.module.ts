508d25ed8fe2a260afa1d5e203b33266d05f649b
Vilhelm <47038708+ViPa222dv@users.noreply.github.com>
2021-06-15 20:36:03 +0200

    Added some simple routing in angular

--- PracticingDB/AngularPractice/AngularPractice/src/app/app.module.ts
+++ PracticingDB/AngularPractice/AngularPractice/src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, RoutingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeMenuComponent } from './components/home-menu/home-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeMenuComponent
+    RoutingComponents
   ],
   imports: [
     BrowserModule,
